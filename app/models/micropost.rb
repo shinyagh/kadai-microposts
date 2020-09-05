78c7193fb7d9587ba56dd11ddab1219bb580b705
shinya <shinyagit@gmail.com>
2020-09-05 04:07:57 +0000

    add model favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :user
+  has_many :addedusers, thorough: :favorites, source :user
   
   validates :content, presence: true, length: { maximum: 255 }
 end
