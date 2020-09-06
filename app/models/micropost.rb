cab1abb4232eb24a6df4c4fe68135ae1e22c9da7
shinya <shinyagit@gmail.com>
2020-09-06 02:58:45 +0000

    fix micropost user

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,20 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :user
-  has_many :addedusers, thorough: :favorites, source :user
+  has_many :favorites
+  has_many :addedusers, thorough: :favorites, source: :user
+  
+  def addfavorite(user)
+    favorites.find_or_create_by(user_id: user.id)
+  end
+  
+  def removefavorite(user)
+    favorite = favorites.find_by(user_id: user.id)
+    favorite.destroy if favorite
+  end  
+  
+  def added?(user)
+    addedusers.include?(user)
+  end
   
   validates :content, presence: true, length: { maximum: 255 }
 end
