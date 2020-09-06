19d425b4a7d0236abf31478d85d0e765d2f8f16d
shinya <shinyagit@gmail.com>
2020-09-06 07:34:02 +0000

    fix model micropost user

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,20 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorites
-  has_many :addedusers, thorough: :favorites, source: :user
-  
-  def addfavorite(user)
-    favorites.find_or_create_by(user_id: user.id)
-  end
-  
-  def removefavorite(user)
-    favorite = favorites.find_by(user_id: user.id)
-    favorite.destroy if favorite
-  end  
-  
-  def added?(user)
-    addedusers.include?(user)
-  end
+  has_many :likedusers, thorough: :favorites, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   has_many :favorites
-  has_many :addedfavorites, through: :favorites, source: :micropost
+  has_many :likes, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -31,5 +31,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def added?(micropost)
+    likes.include?(micropost)
   end
 end
